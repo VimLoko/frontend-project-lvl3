806e39a874ed0d1332b74610a1613c030bbd3d61
Proggit <proggit@mail.ru>
2021-12-20 20:48:43 +0500

    step 7

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-  },
+  entry: path.resolve(__dirname, './src/index.js'),
+  // output: {
+  //   path: path.resolve(__dirname, './dist'),
+  // },
   module: {
     rules: [
       {
